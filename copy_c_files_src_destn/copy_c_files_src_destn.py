f05ef2434cfe055a3f73a763372a91705301a6bd
aruns <aruns.mail27@gmail.com>
2021-01-02 16:04:48 +0530

    [Modifed to update different file types]

--- copy_c_files_src_destn/copy_c_files_src_destn.py
+++ copy_c_files_src_destn/copy_c_files_src_destn.py
@@ -1,3 +1,8 @@
+#Author: Arun
+#Given a folder containing different types of files.
+#Group all files based on their extension (e.g *.pdf,*.c, *.docx)
+#in their respective directories.
+
 import os
 import shutil
 import glob
@@ -7,14 +12,46 @@ ProjectName = 'test'
 TopView = os.getcwd()
 print ("TopView %s",TopView)
 
-# Directory 
+# Path
 directory = "c_source"
-  
-# Path 
-path = os.path.join(TopView, directory)
-os.mkdir(path) 
-
+dest_folder = os.path.join(TopView, directory)
+if not os.path.exists(dest_folder):
+       os.mkdir(dest_folder) 
 for filename in glob.glob(os.path.join(TopView, '*.c')):
-    shutil.copy(filename, dest_folder)
+    shutil.move(filename, dest_folder)
 
+directory = "pdf_docs"
+dest_folder = os.path.join(TopView, directory)
+if not os.path.exists(dest_folder):
+       os.mkdir(dest_folder)
+for filename in glob.glob(os.path.join(TopView, '*.pdf')):
+    shutil.move(filename, dest_folder)
+
+directory = "wordx_docs"
+dest_folder = os.path.join(TopView, directory)
+if not os.path.exists(dest_folder):
+       os.mkdir(dest_folder) 
+for filename in glob.glob(os.path.join(TopView, '*.docx')):
+    shutil.move(filename, dest_folder)
+
+directory = "text_docs"
+dest_folder = os.path.join(TopView, directory)
+if not os.path.exists(dest_folder):
+       os.mkdir(dest_folder) 
+for filename in glob.glob(os.path.join(TopView, '*.txt')):
+    shutil.move(filename, dest_folder)
+
+directory = "odt_docs"
+dest_folder = os.path.join(TopView, directory)
+if not os.path.exists(dest_folder):
+       os.mkdir(dest_folder) 
+for filename in glob.glob(os.path.join(TopView, '*.odt')):
+    shutil.move(filename, dest_folder)     
+
+directory = "junk_docs"
+dest_folder = os.path.join(TopView, directory)
+if not os.path.exists(dest_folder):
+       os.mkdir(dest_folder) 
+for filename in glob.glob(os.path.join(TopView, '*.*')):
+    shutil.move(filename, dest_folder)     
 
